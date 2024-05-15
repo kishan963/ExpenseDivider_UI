4310b73fd916f2b84b2046e961c8d418e439aaca
Kishan Agrawal <kishsnagrawal963@gmail.com>
2024-05-16 00:07:38 +0530

    Disbaled the button when api call is happening

--- src/Pages/AddGroup.jsx
+++ src/Pages/AddGroup.jsx
@@ -1,131 +1,88 @@
-
-
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-// import { Label } from "@/components/ui/label"
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import ApiCalls from "@/Api/ApiCalls"
-import MultipleSelect from "@/components/ui/Dropdown"
-import { useState } from "react"
-import { useNavigate } from "react-router-dom"
-
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import ApiCalls from "@/Api/ApiCalls";
+import MultipleSelect from "@/components/ui/Dropdown";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import CircularIndeterminate from "@/components/ui/Progress";
 
 // schema for login form 
 const loginFormSchema = z.object({
   Name: z.string().min(1, {
     message: "Name must be at least 1 characters.",
   })
-})
-
+});
 
 function AddGroupHandler() {
   const navigate = useNavigate();
   const [selectedNames, setSelectedNames] = useState([]);
+  const [isLoading, setIsLoading] = useState(false); // State to track loading status
+
   const handleNameChanges = (event) => {
     setSelectedNames(event.target.value);
   };
-  // 1. Define your loginForm.
+
   const loginForm = useForm({
     resolver: zodResolver(loginFormSchema),
-    defaultValues: {
-      Name: ""
-    },
-    placeholder: {
-      Name: "Enter Desc"
-    }
-  })
-  // 2. Define a submit handler.
-  function onSubmit(values) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
-    console.log(selectedNames)
-    const body = {
-      Name: values.Name,
-      Users: selectedNames
-    }
-    console.log(body)
-    addExpenseAndNavigate(body);
-   
-    
-    
-}
+    defaultValues: { Name: "" },
+    placeholder: { Name: "Enter Desc" }
+  });
 
-async function addExpenseAndNavigate(body) {
-  try {
-    await ApiCalls.CreateGroupHandler(body); // Wait for the API call to finish
-    navigate(`/home`); // Navigate after the API call is finished
-  } catch (error) {
-    console.error('Error adding expense:', error);
-    // Handle error if needed
+  async function onSubmit(values) {
+    setIsLoading(true); // Set loading state to true when submitting
+    try {
+      const body = { Name: values.Name, Users: selectedNames };
+      await ApiCalls.CreateGroupHandler(body);
+      navigate(`/home`);
+    } catch (error) {
+      console.error('Error adding expense:', error);
+    }
+    setIsLoading(false); // Set loading state to false when API call finishes
   }
+
+  return (
+    <div className="w-screen max-h-full flex justify-center items-center">
+      <Card className="w-screen max-h-full overflow-auto">
+        <CardHeader>
+          <CardTitle>Create Group</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form {...loginForm}>
+            <form onSubmit={loginForm.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={loginForm.control}
+                name="Name"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Name</FormLabel>
+                    <FormControl>
+                      <Input placeholder="shadcn" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormItem>
+                <FormLabel>PaidTo</FormLabel>
+                <FormControl>
+                  <MultipleSelect GroupId={null} selectedNames={selectedNames} onNameChange={handleNameChanges} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+              <CardFooter className="flex justify-between">
+                <Button type="submit" disabled={isLoading}> { isLoading==false ? "Create Group" : <CircularIndeterminate/>}</Button> {/* Disable button if isLoading is true */}
+              </CardFooter>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 
-return (
-  <div className="w-screen max-h-full flex justify-center items-center">
-    <Card className="w-screen max-h-full  overflow-auto">
-      <CardHeader>
-        <CardTitle>Create Group</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <Form {...loginForm}>
-          <form onSubmit={loginForm.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={loginForm.control}
-              name="Name"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Name</FormLabel>
-                  <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-           
-
-            <FormItem>
-              <FormLabel>PaidTo</FormLabel>
-              <FormControl>
-                <MultipleSelect GroupId={null} selectedNames={selectedNames} onNameChange={handleNameChanges} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-
-
-
-
-            <CardFooter className="flex justify-between">
-              <Button type="submit">Create Group</Button>
-            </CardFooter>
-          </form>
-        </Form>
-      </CardContent>
-
-    </Card>
-
-  </div>
-
-);
-
-}
-
-export default AddGroupHandler
+export default AddGroupHandler;
--- src/Pages/Expense.jsx
+++ src/Pages/Expense.jsx
@@ -1,32 +1,16 @@
-
-
-import { Button } from "@/components/ui/button"
-import {
-  Card,
-  CardContent,
-  CardFooter,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
-import { Input } from "@/components/ui/input"
-// import { Label } from "@/components/ui/label"
-import {
-  Form,
-  FormControl,
-  FormField,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "@/components/ui/form"
-import { zodResolver } from "@hookform/resolvers/zod"
-import { useForm } from "react-hook-form"
-import { z } from "zod"
-import ApiCalls from "@/Api/ApiCalls"
-import MultipleSelect from "@/components/ui/Dropdown"
-import { useState } from "react"
-import SingleSelect from "@/components/ui/SingleSelect"
-import { useNavigate, useParams } from "react-router-dom"
-
+import { Button } from "@/components/ui/button";
+import { Card, CardContent, CardFooter, CardHeader, CardTitle } from "@/components/ui/card";
+import { Input } from "@/components/ui/input";
+import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import ApiCalls from "@/Api/ApiCalls";
+import MultipleSelect from "@/components/ui/Dropdown";
+import SingleSelect from "@/components/ui/SingleSelect";
+import { useState } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import CircularIndeterminate from "@/components/ui/Progress";
 
 // schema for login form 
 const loginFormSchema = z.object({
@@ -36,22 +20,23 @@ const loginFormSchema = z.object({
   Amount: z.string().min(1, {
     message: "Amount must be at least 1 digits"
   })
-})
-
+});
 
 function ExpenseHandler() {
   const { id } = useParams();
   const navigate = useNavigate();
   const [selectedNames, setSelectedNames] = useState([]);
+  const [selectedName, setSelectedName] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+
   const handleNameChanges = (event) => {
     setSelectedNames(event.target.value);
   };
 
-  const [selectedName, setSelectedName] = useState([]);
   const handleNameChange = (event) => {
     setSelectedName(event.target.value);
   };
-  // 1. Define your loginForm.
+
   const loginForm = useForm({
     resolver: zodResolver(loginFormSchema),
     defaultValues: {
@@ -62,109 +47,89 @@ function ExpenseHandler() {
       Description: "Enter Desc",
       Amount: "Enter Amount"
     }
-  })
-  // 2. Define a submit handler.
-  function onSubmit(values) {
-    // Do something with the form values.
-    // ✅ This will be type-safe and validated.
-    console.log(values)
-    console.log(selectedName)
-    console.log(selectedNames)
-    const body = {
-      Description: values.Description,
-      Amount: JSON.parse(values.Amount),
-      GroupId: JSON.parse(id),
-      PaidBy_User: selectedName.Id,
-      Users: selectedNames
+  });
+
+  async function onSubmit(values) {
+    setIsLoading(true); // Set loading state to true before making the API call
+    try {
+      const body = {
+        Description: values.Description,
+        Amount: JSON.parse(values.Amount),
+        GroupId: JSON.parse(id),
+        PaidBy_User: selectedName.Id,
+        Users: selectedNames
+      };
+      await ApiCalls.AddExpenseHandler(body); // Wait for the API call to finish
+      navigate(`/group/${id}`); // Navigate after the API call is finished
+    } catch (error) {
+      console.error('Error adding expense:', error);
     }
-    console.log(body)
-    addExpenseAndNavigate(body);
-   
-    
-    
-}
-
-async function addExpenseAndNavigate(body) {
-  try {
-    await ApiCalls.AddExpenseHandler(body); // Wait for the API call to finish
-    navigate(`/group/${id}`); // Navigate after the API call is finished
-  } catch (error) {
-    console.error('Error adding expense:', error);
-    // Handle error if needed
+    setIsLoading(false); // Set loading state to false after the API call is completed
   }
+
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <Card className="w-screen max-h-full overflow-auto">
+        <CardHeader>
+          <CardTitle>Add Expense</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <Form {...loginForm}>
+            <form onSubmit={loginForm.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={loginForm.control}
+                name="Description"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Description</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter description" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+              <FormField
+                control={loginForm.control}
+                name="Amount"
+                render={({ field }) => (
+                  <FormItem>
+                    <FormLabel>Amount</FormLabel>
+                    <FormControl>
+                      <Input placeholder="Enter amount" {...field} />
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+
+              <FormItem>
+                <FormLabel>PaidBy</FormLabel>
+                <FormControl>
+                  <SingleSelect GroupId={id} selectedNames={selectedName} onNameChange={handleNameChange} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+
+              <FormItem>
+                <FormLabel>PaidTo</FormLabel>
+                <FormControl>
+                  <MultipleSelect GroupId={id} selectedNames={selectedNames} onNameChange={handleNameChanges} />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+
+              <CardFooter className="flex justify-between">
+                <Button type="submit" disabled={isLoading}>
+                  { isLoading ? <CircularIndeterminate /> : "Submit" }
+                </Button>
+              </CardFooter>
+            </form>
+          </Form>
+        </CardContent>
+      </Card>
+    </div>
+  );
 }
 
-return (
-  <div className="w-screen h-screen flex justify-center items-center">
-    <Card className="w-screen max-h-full overflow-auto">
-      <CardHeader>
-        <CardTitle>Add Expense</CardTitle>
-      </CardHeader>
-      <CardContent>
-        <Form {...loginForm}>
-          <form onSubmit={loginForm.handleSubmit(onSubmit)} className="space-y-8">
-            <FormField
-              control={loginForm.control}
-              name="Description"
-              render={({ field }) => (
-                <FormItem>
-                  <FormLabel>Description</FormLabel>
-                  <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-            />
-            <FormField
-              control={loginForm.control}
-              name="Amount"
-              render={({ field }) => (
-                <FormItem>
-                  {console.log(field)}
-                  <FormLabel>Amount</FormLabel>
-                  <FormControl>
-                    <Input placeholder="shadcn" {...field} />
-                  </FormControl>
-                  <FormMessage />
-                </FormItem>
-              )}
-
-
-            />
-
-            <FormItem>
-              <FormLabel>PaidBy</FormLabel>
-              <FormControl>
-                <SingleSelect GroupId={id} selectedNames={selectedName} onNameChange={handleNameChange} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-
-            <FormItem>
-              <FormLabel>PaidTo</FormLabel>
-              <FormControl>
-                <MultipleSelect GroupId={id} selectedNames={selectedNames} onNameChange={handleNameChanges} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-
-
-
-
-            <CardFooter className="flex justify-between">
-              <Button type="submit">Submit</Button>
-            </CardFooter>
-          </form>
-        </Form>
-      </CardContent>
-
-    </Card>
-
-  </div>
-
-);
-
-}
-
-export default ExpenseHandler
+export default ExpenseHandler;
