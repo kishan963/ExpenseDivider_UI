17e7cf594aa31959ddbaa00eb986db1b7624c21f
Kishan Agrawal <kishsnagrawal963@gmail.com>
2024-08-03 15:00:27 +0530

    Home page was blank when api call was taking time. Added Please Wait text so that user know page is loading.

--- src/Pages/Loading.jsx
+++ src/Pages/Loading.jsx
@@ -1,12 +1,13 @@
 
 
 import ApiCalls from "@/Api/ApiCalls";
-import { useEffect } from "react";
+import {useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
 
 
 
 const CheckToken = ()=>{
+    const [loading, setLoading] = useState(true);
     const navigate= useNavigate();
     // React.useEffect(() => {
     //     const retrievedData = localStorage.getItem('token_splitwise');
@@ -25,11 +26,25 @@ const CheckToken = ()=>{
         } catch (err) {
             navigate("/login")
         }
+        finally {
+            setLoading(false); // Set loading to false once the API call is done
+        }
     };
 
     useEffect(() => {
         fetchData();
     }, []);
+
+
+    return (
+        <div>
+            {loading ? (
+                <p>Please wait...</p> // Display the "Please wait" message
+            ) : (
+                <p>Content has loaded</p> // This is just a placeholder, replace with your actual content
+            )}
+        </div>
+    );
 
 }
 
